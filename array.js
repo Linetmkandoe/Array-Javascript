11dc7785480e52062b4e9a03e38802c182d0ca1f
Linet Ndau <mkandoelinet@gmail.com>
2024-03-21 19:24:27 +0300

    add a file

---
+++ array.js
@@ -0,0 +1,16 @@
+let arr1 = [3,7,34,90,12]
+let lastelement1 = arr1.slice(-1)
+console.log(lastelement1);
+
+let arr2 = [true,"green","where",12,56]
+let lastelement2 = arr2.slice(-1)
+console.log(lastelement2)
+
+
+const myPets = ["Cow","Bird","Snake","Dog"];
+console.log(myPets.join())
+
+
+const numbers = [-5,9,5,3,2,-3,6,8,4,1];
+numbers.sort();
+console.log(numbers);
